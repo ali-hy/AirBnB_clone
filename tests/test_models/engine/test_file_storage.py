8e627e719e862810054c78abff6ff127fdfd8b67
ali-hy <ali.haitham.ymy@gmail.com>
2024-03-10 09:48:10 +0200

    more tests for file storage

--- tests/test_models/engine/test_file_storage.py
+++ tests/test_models/engine/test_file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
@@ -14,6 +15,7 @@ class TestFileStorage(unittest.TestCase):
     def test_all(self):
         """Test all method of FileStorage instance"""
         self.assertIsInstance(self.storage.all(), dict)
+        self.assertDictEqual(self.storage, FileStorage.__objects)
 
     def test_new(self):
         """Test new method of FileStorage instance"""
@@ -21,12 +23,16 @@ class TestFileStorage(unittest.TestCase):
         self.storage.new(obj)
         key = obj.__class__.__name__ + "." + obj.id
         self.assertIn(key, self.storage.all())
+        self.assertDictEqual(obj.to_dict(), self.storage.all()[key].to_dict())
 
     def test_save_and_reload(self):
         """Test save and reload methods of FileStorage instance"""
-        obj = BaseModel()
+        obj = BaseModel(id='abc',
+                        created_at=datetime.now().isoformat(),
+                        updated_at=datetime.now().isoformat())
         self.storage.new(obj)
         self.storage.save()
         self.storage.reload()
         key = obj.__class__.__name__ + "." + obj.id
         self.assertIn(key, self.storage.all())
+        self.assertDictEqual(obj.to_dict(), self.storage.all()[key].to_dict())
